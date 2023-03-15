46890ccbc23b818ff1c8578dce3f45b0b6c8ab35
mk0029 <mkhicher404@gmail.com>
2023-03-15 10:43:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Header from "./components/Header";
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
